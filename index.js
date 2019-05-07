e49a7ece636223f23f38803245e4d7bcf291b96a
Didre <ixchel@hotmail.es>
2019-05-07 03:03:20 -0500

    cambios package.json y funciones de practica

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-module.exports = () => {
-  // ...
+module.exports.saludar = () => {
+   console.log('Hola Mundo');
+  
 };
-console.log('Hola, Mundo!')
+
+const os = require ('os');
+//const fs = require ('fs');
+
+let cpu = os.cpus();
+let sistema = os.plataform();
+let usuario = os.hostname();
+
+console.log(cpu);
+console.log(sistema)
+console.log(usuario);
+
+//fs.appendFile('mitocode.txt','Informacion del cpu: ' + cpu,function (error) {
+//    if(error){
+//    console.log('Error al crear archivo');
+//    }
+//})
+
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -12,6 +12,13 @@
 // __tests__/index.test.js
 describe('initial', () => {
   test('first tests', () => {
-    expect(true).toBe(true)
-  })
+     expect(true).toBe(true)
+ })
 })
+
+//Test de links
+require("./README.md");
+  .then(links => {
+    // => [{ href, text, file }]
+  })
+  .catch(console.error);
