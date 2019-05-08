ecfc6c3e35b85543f882171c4aeca6e252783028
Didre <ixchel@hotmail.es>
2019-05-07 22:00:12 -0500

    El resultado se node en terminal muestra resultados de los objetos

--- index.js
+++ index.js
@@ -1,26 +1,47 @@
-
-//librerias
-const fs = require('fs');
-const path = require('path');
-
-//const saludar = (path) =>{
-//   return console.log("Hola mundo");
-//};
-
-/**Función que valida si existe una ruta de Archivo */
-const pathInserted = (pathFile) => {
-   if(pathFile == undefined){
-      
-      return false;
-   }
-   else{
-      return true;
-   }
-};
-
-module.exports = {
-   'pathInserted': pathInserted,
-   
-};
+   //librerias
+   const fs = require('fs');
+   const path = require('path');
+   const filePath = process.argv[2];
+   const readPath = fs.readFileSync(filePath, 'utf8');
 
 
+   //const saludar = (path) =>{
+   //   return console.log("Hola mundo");
+   //};
+
+   //Función que valida si existe una ruta de Archivo 
+   const pathInserted = (pathFile) => {
+     if (pathFile == undefined) {
+       return false;
+     } else {
+       return true;
+     }
+   };
+
+
+   //Función para extraer links de archivo MD
+   const extractorInf = (extract) => {
+     const regularExpression = /\[(.*?)\]\((.*?)\)/gi;
+     let haveArray = [];
+     //El método match() con búsqueda global (modificador "g") busca una o todas las coincidencias
+     // de una expresión regular sobre un String, devolviéndolas en un array
+     while (match = regularExpression.exec(extract)) {
+       //método exec(),se ejecuta sobre la expresión regular, siendo su argumento el String. Con una búsqueda global 
+       //y con cada ejecución modificará las propiedades 
+       arrayContent = {
+         href: match[2],
+         text: match[1]
+       };
+       haveArray.push(arrayContent);
+     };
+     return haveArray;
+   };
+
+   //Nos muestra en la terminal array de links
+   console.log(extractorInf(readPath));
+   console.log('found links:' + extractorInf(readPath).length);
+
+   module.exports = {
+     'pathInserted': pathInserted,
+     'extractorInf': extractorInf,
+   };
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,14 +1,15 @@
 const mdLinks = require('../index.js');
 
-/**Primer test, verifica que exista una ruta de archivo */
+//Primer test, verifica que exista una ruta de archivo 
 describe('pathInserted', () => {
   test('Should be a function', () => {
-     expect(typeof mdLinks.pathInserted).toBe('function');
+    expect(typeof mdLinks.pathInserted).toBe('function');
   });
-    it('Should return false',() => {
-      expect(mdLinks.pathInserted()).toBe(false);
+  it('Should return false', () => {
+    expect(mdLinks.pathInserted()).toBe(false);
   });
-it('Should return true',() => {
-  expect(mdLinks.pathInserted('./README.md')).toBe(true);
+  it('Should return true', () => {
+    expect(mdLinks.pathInserted('./README.md')).toBe(true);
   });
 });
+
